7433368b39f2edd1840a7c2bc643f486cb44911f
bereczkybalazs <bereczkybalazs1@gmail.com>
2018-08-17 10:09:23 +0200

    removed versioning from blade dependencies

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,12 +12,12 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <div id="app">
         <app></app>
     </div>
-    <script src="{{ mix('/js/app.js') }}"></script>
+    <script src="/js/app.js"></script>
 </body>
 </html>
